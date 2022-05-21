b093a5fe6ba9f0bc7fe461c367f68218ed5d4ace
Rohit <96435264+Rohitnith@users.noreply.github.com>
2022-05-21 17:13:02 +0530

    Add files via upload

---
+++ Factorial/Factorial.py
@@ -0,0 +1,9 @@
+a=int(input("enter No. of whom factorial is required"))
+fac=1
+for i in range(1, a+1):
+    fac=fac*i
+    if i<=a:
+        Continue
+print("Factorial(!) of", a,"is", fac)
+
+print("Thanks for using Python")
